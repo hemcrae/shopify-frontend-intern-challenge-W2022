47ed6cd9c7f7ee512600cbfde6f63b7452bd738f
hemcrae <halley.mcrae@gmail.com>
2021-09-13 09:40:58 -0400

    added logic to Read More button

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './Card.module.scss'
+
 
 interface CardProps { 
   url: string
@@ -9,6 +10,9 @@ interface CardProps {
 }
 
 export const Card: React.FC<CardProps> = ({ url, title, date, explanation }) => {
+
+  const [showElement, setShowElement] = useState(false);
+  
   return ( 
     <article className={classes.card}>
       <img className={classes.image} src={url} alt="Astronomy Picture of the Day"/>
@@ -20,11 +24,11 @@ export const Card: React.FC<CardProps> = ({ url, title, date, explanation }) => 
           {date}
         </time>
       </header>
-      <p className={classes.explanation}>
-        {explanation}
-      </p>
+        <p className={`${classes.explanation} ${showElement ? classes.showExplanation : ''}`}>
+          {explanation}
+        </p>
       <footer className={classes.footer}>
-        <button className={classes.expandButton}>
+        <button className={classes.expandButton} onClick={() => setShowElement(!showElement)}>
           Read More
         </button>
         <button className={classes.likeButton}>
